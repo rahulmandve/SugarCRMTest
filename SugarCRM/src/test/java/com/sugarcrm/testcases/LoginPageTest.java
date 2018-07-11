7879fadcc7da50cabbfc89d2272601a01d2e163b
rahulmandve <rahul.mandwe259@gmail.com>
2018-07-11 16:55:47 +0530

    second commit

--- SugarCRM/src/test/java/com/sugarcrm/testcases/LoginPageTest.java
+++ SugarCRM/src/test/java/com/sugarcrm/testcases/LoginPageTest.java
@@ -1,5 +1,8 @@
 package com.sugarcrm.testcases;
-
+/*
+ * Author Rahul Mandve
+ * 
+ */
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
